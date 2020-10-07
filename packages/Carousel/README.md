fd23b8d4afaaeac1d2fd8260f80ed05f890bd509
shlroland <shlroland1995@gmail.com>
2020-10-07 10:15:27 +0800

    feat: init carousel component

---
+++ packages/Carousel/README.md
@@ -0,0 +1,11 @@
+# `Carousel`
+
+> TODO: description
+
+## Usage
+
+```
+const carousel = require('Carousel');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/Carousel/src/carousel-item.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'RolCarouselItem',
+})
+</script>
+
+<style></style>
