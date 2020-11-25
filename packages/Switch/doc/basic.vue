920f06092b8c050ec694867bbaa49e2c761bfa2e
shlroland <shlroland1995@gmail.com>
2020-11-25 11:34:17 +0800

    feat(switch): basic switch transition

---
+++ packages/Switch/doc/basic.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="demo-container">
+    <rol-switch v-model:modelValue="value" active-color="#13ce66" inactive-color="#ff4949"></rol-switch>
+  </div>
+</template>
+
+<script>
+import RolSwitch from '@rol-ui/switch'
+
+export default {
+    components: { RolSwitch },
+    data(){
+      return {
+        value: false
+      }
+    }
+}
+</script>
+
+<style>
+</style>
