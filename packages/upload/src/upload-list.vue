0af5988a979da01773d17384ee04a234912aa0de
shlroland <shlroland1995@gmail.com>
2020-11-29 19:12:17 +0800

    fix(upload): fix close-tip

--- packages/upload/src/upload-list.vue
+++ packages/upload/src/upload-list.vue
@@ -38,7 +38,7 @@
         <span v-if="!disabled" class="rol-icon-close">
           <rol-icon :name="['far', 'times-circle']" @click="handleRemove(file)"></rol-icon>
         </span>
-        <i v-if="!disabled && listType === 'text'" class="rol-icon-close-tip">按delete可删除</i>
+        <i v-if="!disabled" class="rol-icon-close-tip">按delete可删除</i>
         <rol-progress
           v-if="file.status === 'uploading' && listType === 'text'"
           :stroke-width="2"
