f44e65599bcc63a8b69aed6f26a9fb5fb3287dd0
shlroland <shlroland1995@gmail.com>
2020-12-12 14:26:27 +0800

    feat(date-picker): add picker-panel scss

---
+++ packages/DatePicker/doc/basic.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo-container">
+    <rol-date-picker v-model="value1" type="date" placeholder="选择日期"></rol-date-picker>
+  </div>
+</template>
+
+<script>
+import RolDatePicker from '@rol-ui/date-picker'
+
+export default {
+  name: 'basic',
+  components: {
+    RolDatePicker,
+  },
+  data() {
+    return {
+      value1: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- packages/DatePicker/src/date-picker.ts
+++ packages/DatePicker/src/date-picker.ts
@@ -1,6 +1,11 @@
 import { DEFAULT_FORMATS_DATE, DEFAULT_FORMATS_DATEPICKER } from '@rol-ui/utils/time-constant'
 import { CommonPicker } from '@rol-ui/time-picker'
 import { defineComponent, h } from 'vue'
+import DatePickerPanel from './components/panel-date-picker.vue'
+
+const getPanel = (type:string) =>{
+    return DatePickerPanel
+}
 
 export default defineComponent({
   name: 'RolDatePicker',
@@ -17,6 +22,8 @@ export default defineComponent({
         format,
         type: props.type,
         ...props,
+      },{
+          default: scopedProps => h(getPanel(props.type), scopedProps),
       })
   },
 })
