99c1bc145d2ee5c9211e9bd9e7e304c704a157e5
shlroland <shlroland1995@gmail.com>
2021-01-03 23:28:20 +0800

    perf(router): have a try about dynamic route

--- docs/router/data-routes.ts
+++ docs/router/data-routes.ts
@@ -1,70 +1,70 @@
 import { routeChild } from './types'
-import Avatar from '/@packages/Avatar/README.md'
-import Image from '/@packages/Image/README.md'
-import Badge from '/@packages/Badge/README.md'
-import Tag from '/@packages/Tag/README.md'
-import Card from '/@packages/Card/README.md'
-import PageHeader from '/@packages/PageHeader/README.md'
-import Carousel from '/@packages/Carousel/README.md'
-import Collapse from '/@packages/Collapse/README.md'
-import Tooltip from '/@packages/Tooltip/README.md'
-import Popover from '/@packages/Popover/README.md'
-import Popconfirm from '/@packages/Popconfirm/README.md'
+// import Avatar from '/@packages/Avatar/README.md'
+// import Image from '/@packages/Image/README.md'
+// import Badge from '/@packages/Badge/README.md'
+// import Tag from '/@packages/Tag/README.md'
+// import Card from '/@packages/Card/README.md'
+// import PageHeader from '/@packages/PageHeader/README.md'
+// import Carousel from '/@packages/Carousel/README.md'
+// import Collapse from '/@packages/Collapse/README.md'
+// import Tooltip from '/@packages/Tooltip/README.md'
+// import Popover from '/@packages/Popover/README.md'
+// import Popconfirm from '/@packages/Popconfirm/README.md'
 
 const menuConfig: routeChild[] = [
   {
     name: 'Avatar 头像',
-    component: Avatar,
+    component: import('/@packages/Avatar/README.md'),
     path: 'grid',
   },
   {
     name: 'Image 图像',
-    component: Image,
+    component: import('/@packages/Image/README.md'),
     path: 'image',
   },
   {
     name: 'Badge 角标',
-    component: Badge,
+    component: import('/@packages/Badge/README.md'),
     path: 'badge',
   },
   {
     name: 'Tag 标签',
-    component: Tag,
+    component: import('/@packages/Tag/README.md'),
     path: 'tag',
   },
   {
     name: 'Card 卡片',
-    component: Card,
+    component: import('/@packages/Card/README.md'),
     path: 'card',
   },
   {
     name: 'PageHeader 页头',
-    component: PageHeader,
+    component: import('/@packages/PageHeader/README.md'),
     path: 'pageHeader',
   },
   {
     name: 'Carousel 轮播',
-    component: Carousel,
+    component: import('/@packages/Carousel/README.md'),
     path: 'carousel',
   },
   {
     name: 'Collapse 折叠面板',
-    component: Collapse,
+    component: import('/@packages/Collapse/README.md'),
     path: 'collapse',
   },
   {
     name: 'Tooltip 文字提示',
-    component: Tooltip,
+    component: import('/@packages/Tooltip/README.md'),
     path: 'tooltip',
   },
   {
     name: 'Popover 气泡卡片',
-    component: Popover,
+    component: import('/@packages/Popover/README.md'),
     path: 'popover',
   },
   {
     name: 'Popconfirm 气泡确认框',
-    component: Popconfirm,
+    component: import('/@packages/Popconfirm/README.md'),
     path: 'popconfirm',
   },
 ]
