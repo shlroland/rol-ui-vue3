db109e06deaf5f623d5aa23c1da5c70175b17d0b
shlroland <shlroland1995@gmail.com>
2021-01-05 20:07:32 +0800

    chore(storybook): delete storybook

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    stories: ['../packages/**/doc/*.stories.[tj]s'],
-    addons: ['@storybook/addon-storysource'],
-    presets: [path.resolve(__dirname, './custom-presets')],
-}
