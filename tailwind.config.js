54063404515879e2cadf28601fdba4c21c58303b
shlroland <shlroland1995@gmail.com>
2021-01-05 13:31:45 +0800

    docs(docs): vite build success

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './packages/**/*.{vue,js,ts,jsx,tsx}','./docs/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
