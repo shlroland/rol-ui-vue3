b64324df68f70bed8713be6da98ce67de0fc8153
shlroland <shlroland1995@gmail.com>
2021-01-10 19:51:30 +0800

    ci: try print env.TAG_VERSION

--- build/gen-version.js
+++ build/gen-version.js
@@ -1,7 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs')
 const path = require('path')
+const tagVer = process.env.TAG_VERSION
 const { version } = require('../package.json')
-
-fs.writeFileSync(path.resolve(__dirname, '../packages/rol-ui/version.ts'), `export const version = '${version}'
-`)
+if (tagVer) {
+  version = tagVer.startsWith('v') ? tagVer.slice(1) : tagVer
+} else {
+  version = require('../package.json').version
+}
+fs.writeFileSync(
+  path.resolve(__dirname, '../packages/rol-ui/version.ts'),
+  `export const version = '${version}'
+`,
+)
